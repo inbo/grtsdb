6b7f1617ffdfc62cddc3207e689a73083481a7db
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2019-05-10 16:05:35 +0200

    add create_index()

---
+++ R/create_index.R
@@ -0,0 +1,19 @@
+#' Add the index to the table
+#' @inheritParams add_level
+#' @export
+#' @importFrom assertthat assert_that is.count
+#' @importFrom RSQLite dbListTables dbListFields dbSendStatement dbClearResult
+create_index <- function(grtsdb, level) {
+  assert_that(is_grtsdb(grtsdb), is.count(level))
+  if (!sprintf("level%02i", level) %in% dbListTables(grtsdb$con)) {
+    stop(sprintf("level %i is not available. use add_level()", level))
+  }
+  fields <- dbListFields(grtsdb$conn, sprintf("level%02i", level))
+  fields <- fields[grep("^x[[:digit:]]*$", fields)]
+  sql <- sprintf(
+    "CREATE INDEX IF NOT EXISTS idx%02i ON level%1$02i (ranking, %s)", level,
+    paste(fields, collapse = ", "))
+  res <- dbSendStatement(grtsdb$conn, sql)
+  dbClearResult(res)
+  return(invisible(grtsdb))
+}
