7424069095db2369a2a7eccc007fb8c3f4e3259f
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2021-12-21 11:21:31 +0100

    update unit tests

--- tests/testthat/test_a_basic.R
+++ tests/testthat/test_a_basic.R
@@ -1,31 +1,33 @@
-expect_is(conn <- connect_db(":memory:"), "SQLiteConnection")
-bbox <- rbind(c(0, 7), c(0, 7))
-cellsize <- 1
-expect_message(
-  add_level(grtsdb = conn, bbox = bbox, cellsize = cellsize),
-  "Required number of levels"
-)
-expect_null(compact_db(grtsdb = conn))
-expect_false(has_index(grtsdb = conn, level = 1))
-expect_message(
-  create_index(grtsdb = conn, level = 1, bbox = bbox, cellsize = cellsize),
-  "Adding level 1"
-)
-expect_is(
-  extract_sample(grtsdb = conn, samplesize = 10, bbox = bbox,
-                 cellsize = cellsize),
-  "data.frame"
-)
-expect_true(has_index(grtsdb = conn, level = 3))
-expect_false(is_grtsdb(NULL))
-expect_null(add_level(grtsdb = conn, bbox = bbox, cellsize = cellsize))
-expect_message(
-  add_level(grtsdb = conn, bbox = bbox, cellsize = cellsize, level = 5),
-  "Adding level 5"
-)
-expect_is(
-  extract_sample(grtsdb = conn, samplesize = 10, bbox = bbox,
-                 cellsize = cellsize, offset = 20),
-  "data.frame"
-)
-RSQLite::dbDisconnect(conn)
+test_that("basic functionality", {
+  expect_is(conn <- connect_db(":memory:"), "SQLiteConnection")
+  bbox <- rbind(c(0, 7), c(0, 7))
+  cellsize <- 1
+  expect_message(
+    add_level(grtsdb = conn, bbox = bbox, cellsize = cellsize),
+    "Required number of levels"
+  )
+  expect_null(compact_db(grtsdb = conn))
+  expect_false(has_index(grtsdb = conn, level = 1))
+  expect_message(
+    create_index(grtsdb = conn, level = 1, bbox = bbox, cellsize = cellsize),
+    "Adding level 1"
+  )
+  expect_is(
+    extract_sample(grtsdb = conn, samplesize = 10, bbox = bbox,
+                   cellsize = cellsize),
+    "data.frame"
+  )
+  expect_true(has_index(grtsdb = conn, level = 3))
+  expect_false(is_grtsdb(NULL))
+  expect_null(add_level(grtsdb = conn, bbox = bbox, cellsize = cellsize))
+  expect_message(
+    add_level(grtsdb = conn, bbox = bbox, cellsize = cellsize, level = 5),
+    "Adding level 5"
+  )
+  expect_is(
+    extract_sample(grtsdb = conn, samplesize = 10, bbox = bbox,
+                   cellsize = cellsize, offset = 20),
+    "data.frame"
+  )
+  RSQLite::dbDisconnect(conn)
+})
